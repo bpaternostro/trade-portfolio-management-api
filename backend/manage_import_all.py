087764421d4035df3fa80529353f302d0d5f05af
Bruno <bruno.paternostro@gmail.com>
2024-02-26 16:10:07 -0300

    fix: time_interval

--- backend/manage_import_all.py
+++ backend/manage_import_all.py
@@ -1,22 +1,26 @@
 import schedule
 import time
+import os
+from django.core.management import execute_from_command_line
 
-from .apps.manager.management.commands import import_data_from_coinbase, import_data_from_balanz
+# Set the DJANGO_SETTINGS_MODULE environment variable
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'brucetrader.settings')
+TIME_INTERVAL = os.environ.get('TIME_INTERVAL_API_PROCESS')
 
 # Define your tasks
 def task_coinbase():
-    import_data_from_coinbase()
+    execute_from_command_line(['manage.py', 'import_data_from_coinbase'])
 
 def task_balanz_1():
-    import_data_from_balanz(1)
+    execute_from_command_line(['manage.py', 'import_data_from_balanz', 1])
 
 def task_balanz_2():
-    import_data_from_balanz(2)
+    execute_from_command_line(['manage.py', 'import_data_from_balanz', 2])
 
 # Schedule tasks to run every 45 minutes
-schedule.every(5).minutes.do(task_coinbase)
-schedule.every(5).minutes.do(task_balanz_1)
-schedule.every(5).minutes.do(task_balanz_2)
+schedule.every(TIME_INTERVAL).minutes.do(task_coinbase)
+schedule.every(TIME_INTERVAL).minutes.do(task_balanz_1)
+schedule.every(TIME_INTERVAL).minutes.do(task_balanz_2)
 
 # Keep the script running to execute scheduled tasks
 while True:
